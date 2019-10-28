a382a065c12ac09e907fcd281d6f799119c109c3
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-28 02:18:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,8 +4,6 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attributes)
-    attributes.each do |key, value|
-      attributes.send("#{key}=", value )
-    end
+    attributes.each {|key, value| attributes.send("#{key}=", value )}
   end
 end
